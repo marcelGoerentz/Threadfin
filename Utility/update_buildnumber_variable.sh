4318dedfd0e49c6e30295f06d58493562e4b6ded
Marcel Goerentz <m.goerentz@t-online.de>
2024-12-27 21:59:43 +0100

    Update script
    Signed-off-by: Marcel Goerentz <m.goerentz@t-online.de>

--- Utility/update_buildnumber_variable.sh
+++ Utility/update_buildnumber_variable.sh
@@ -9,4 +9,4 @@ curl -X PATCH \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-Github-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$REPOSITORY/actions/variables/BUILD_NUMBER" \
-  -d "{\"value\":\"$NEW_BUILD\"}"
+  -d '{"value":"$NEW_BUILD"}'
