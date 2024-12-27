8507c01a30c03802a82c98741f7c2ae99536e81d
Marcel Goerentz <m.goerentz@t-online.de>
2024-12-27 21:37:06 +0100

    Update script and yml files
    Signed-off-by: Marcel Goerentz <m.goerentz@t-online.de>

--- Utility/update_buildnumber_variable.sh
+++ Utility/update_buildnumber_variable.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+GITHUB_TOKEN=$1
+REPOSITORY=$2
+NEW_BUILD=$3
+
 curl -X PATCH \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-Github-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${{ github.repository }}/actions/variables/BUILD_NUMBER" \
-  -d "{\"value\":\"${{ env.NEW_BUILD }}\"}"
+  "https://api.github.com/repos/$REPOSITORY/actions/variables/BUILD_NUMBER" \
+  -d "{\"value\":\"$NEW_BUILD\"}"
