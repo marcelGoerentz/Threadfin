a3d90ed1f25ef4e29b7c024bc1cf99b6b1166542
Marcel Goerentz <m.goerentz@t-online.de>
2024-12-30 21:51:50 +0100

    Update workflows and scripts
    Signed-off-by: Marcel Goerentz <m.goerentz@t-online.de>

--- Utility/set_build_number.sh
+++ Utility/set_build_number.sh
@@ -7,8 +7,6 @@ fi
 
 new_build=$(($1 + 1))
 
-echo "New buildnumber is: $new_build"
-
 # Extract the version number
 version=$(grep 'const Version' "threadfin.go" | sed 's/.*Version = "\(.*\)"/\1/')
 
@@ -21,8 +19,6 @@ minor=${version_parts[1]}
 patch=${version_parts[2]}
 new_version="$major.$minor.$patch.$new_build"
 
-echo "New version is: $new_version"
-
 # Update the version in the file
 sed -i "s/const Version = \".*\"/const Version = \"$new_version\"/" threadfin.go
 
@@ -31,3 +27,6 @@ echo "NEW_BUILD=$new_build" >> $GITHUB_ENV
 
 # Export the new Version to the GitHub environment
 echo "NEW_VERSION=$new_version" >> $GITHUB_ENV
+
+# Set the output for the GitHub Actions step
+echo "new_version=$new_version" >> $GITHUB_OUTPUT
